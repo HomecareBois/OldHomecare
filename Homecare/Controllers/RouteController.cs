3cb52ea3740bedb023786b8731cd962c4bf52bbf
jonathan lenz <pesti_lenz@hotmail.com>
2018-04-26 12:15:59 +0200

    Route start

---
+++ Homecare/Controllers/RouteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homecare.Controllers
+{
+    public class RouteController : Controller
+    {
+        // GET: Route
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateRoute()
+        {
+            return View();
+        }
+    }
+}
---
+++ Homecare/Models/ViewModels/RouteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homecare.Models.ViewModels
+{
+    public class RouteViewModel
+    {
+        public DateTime date { get; set; }
+
+        public DateTime time { get; set; }
+
+        public string patient { get; set; }
+
+
+    }
+}
