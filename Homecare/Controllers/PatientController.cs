cd8f1e8235910b06dc31a5c9e2f51f3567308a95
Fredrik <fredrik0301@gmail.com>
2018-04-26 11:11:07 +0200

    yoyoyo

--- Homecare/Controllers/PatientController.cs
+++ Homecare/Controllers/PatientController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Homecare.Models.DataModels;
+using Homecare.Models.ViewModels;
 
 namespace Homecare.Controllers
 {
@@ -11,6 +13,71 @@ namespace Homecare.Controllers
         // GET: Patient
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult CreatePatient()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePatient(PatientViewModel inputData)
+        {
+            if (ModelState.IsValid)
+            {
+                using (HomecareDBEntities db = new HomecareDBEntities())
+                {
+                    var c = new City
+                    {
+                        city_name = inputData.cityName,
+                        zipcode = inputData.zipCode
+                    };
+
+                    db.Cities.Add(c);
+                    db.SaveChanges();
+
+                    var cityId = db.Cities.FirstOrDefault(ci => ci.city_name == inputData.cityName).id_city;
+
+                    var phone = new Phone
+                    {
+                        phone_number = inputData.phonenumber,
+                    };
+                    db.Phones.Add(phone);
+                    db.SaveChanges();
+
+                    var phoneId = db.Phones.FirstOrDefault(pi => pi.phone_number == inputData.phonenumber).id_phone;
+
+                    var address = new Address
+                    {
+                        road_name = inputData.roadname,
+                        number = inputData.number,
+                        fk_city_address = cityId
+                    };
+                    db.Addresses.Add(address);
+                    db.SaveChanges();
+
+                    var addressId = db.Addresses
+                        .FirstOrDefault(ai =>
+                        ai.road_name == inputData.roadname &&
+                        ai.number == inputData.number)
+                        .id_address;
+
+                    var p = new Patient
+                    {
+                        patient_name = inputData.name,
+                        cpr = inputData.cpr,
+                        relative_phonenumber = inputData.relativePhonenumber,
+                        fk_address_patient = addressId,
+                        fk_phone_patient = phoneId
+                    };
+
+                    db.Patients.Add(p);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+            }
+
             return View();
         }
     }
---
+++ Homecare/Models/ViewModels/PatientViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homecare.Models.ViewModels
+{
+    public class PatientViewModel
+    {
+        public string name { get; set; }
+        public string cpr { get; set; }
+        public string roadname { get; set; }
+        public string number { get; set; }
+        public string cityName { get; set; }
+        public string zipCode { get; set; }
+        public string phonenumber { get; set; }
+        public string relativePhonenumber { get; set; }
+    }
+}
