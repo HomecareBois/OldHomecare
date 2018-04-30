2f5027f6204faf4e061ea14dc07c601a3a1d867c
jonathan lenz <pesti_lenz@hotmail.com>
2018-04-30 11:47:55 +0200

    Create route prototype

--- Homecare/Controllers/RouteController.cs
+++ Homecare/Controllers/RouteController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Homecare.Models.DataModels;
+using Homecare.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +11,39 @@ namespace Homecare.Controllers
     public class RouteController : Controller
     {
         // GET: Route
-        public ActionResult Index()
+        
+        public ActionResult CreateRoute()
         {
             return View();
         }
-
-        public ActionResult CreateRoute()
+        [HttpPost]
+        public ActionResult CreateRoute(RouteViewModel inputData)
         {
+            using (HomecareDBEntities db = new HomecareDBEntities())
+            {
+                var caretakerIdToDb = db.Caretakers
+                    .FirstOrDefault(ci => ci.caretaker_name == inputData.caretaker)
+                    .id_caretaker;
+
+                var addressIdToDb = db.Patients
+                    .FirstOrDefault(ai => ai.cpr == inputData.patientCpr).fk_address_patient;
+
+                var arrivalToDb = inputData.date.TimeOfDay;
+
+                var dateToDb = inputData.date.Date;
+
+                var route = new Route
+                {
+                    arrival = arrivalToDb,
+                    date = dateToDb,
+                    fk_caretaker_route = caretakerIdToDb,
+                    fk_address_route = addressIdToDb
+
+                };
+                db.Routes.Add(route);
+                db.SaveChanges();
+            };
+
             return View();
         }
     }
--- Homecare/Models/ViewModels/RouteViewModel.cs
+++ Homecare/Models/ViewModels/RouteViewModel.cs
@@ -7,12 +7,14 @@ namespace Homecare.Models.ViewModels
 {
     public class RouteViewModel
     {
+
+        public string caretaker { get; set; }
+
         public DateTime date { get; set; }
 
         public DateTime time { get; set; }
 
-        public string patient { get; set; }
-
-
+        public string patientCpr { get; set; }
+        
     }
 }
