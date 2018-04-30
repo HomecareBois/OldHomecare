d2000bc4a3a2687a1b5b0e2b81596b4b1e63c269
AnderskdKnudsen <anderskdknudsen@gmail.com>
2018-04-30 11:10:33 +0200

    added createcaretaker

--- Homecare/Models/ViewModels/CaretakerViewModel.cs
+++ Homecare/Models/ViewModels/CaretakerViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,18 @@ namespace Homecare.Models.ViewModels
 {
     public class CaretakerViewModel
     {
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Dit kodeord skal bestå af mindst 3 og højst 20 tegn")]
+        public string password { get; set; }
+        [Required]
+        public string user_rights { get; set; }
+        [Required]
+        public string phonenumber { get; set; }
     }
 }
